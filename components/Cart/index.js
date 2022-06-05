50a501a9c1781fccbcf2ddeff5fedb1c37719548
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-06-05 13:32:14 +0900

    56 ショッピングカート用コンポーネントを作成(その2)

--- components/Cart/index.js
+++ components/Cart/index.js
@@ -1,4 +1,5 @@
-import { Card, CardBody, CardTitle } from "reactstrap";
+import { Badge, Button, Card, CardBody, CardTitle } from "reactstrap";
+import Link from "next/link"
 
 const Cart = () => {
   return (
@@ -16,6 +17,28 @@ const Cart = () => {
             <div className="items-one">
               <span id="item-price">&nbsp; 200円</span>
               <span id="item-name">&nbsp; サラダ</span>
+              <div>
+                <Button>
+                  +
+                </Button>
+                <Button>
+                  -
+                </Button>
+                <span id="item-quantity">１つ</span>
+              </div>
+            </div>
+            <div>
+              <Badge>
+                <h5>合計：</h5>
+                <h3>1200円</h3>
+              </Badge>
+              <div>
+                <Link href="/checkout">
+                  <Button>
+                    <a>注文する</a>
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
         </CardBody>
